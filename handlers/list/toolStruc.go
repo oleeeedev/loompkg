f78ba2cf55305b2489c7337e6c66bf3a96f7aa7a
oleeeedev <oleschumacherr1801@gmail.com>
2025-01-24 23:33:46 +0100

    Add initial list of tools to toolStruc.go
    Populated the `tools` variable with a predefined list of tool names. This ensures a baseline set of tools is available for usage within the application.

--- handlers/list/toolStruc.go
+++ handlers/list/toolStruc.go
@@ -4,4 +4,53 @@ type Tool struct {
 	Name string
 }
 
-var tools = []Tool{}
+var tools = []Tool{
+	{
+		Name: "awscli",
+	},
+	{
+		Name: "nmap",
+	},
+	{
+		Name: "nuclei",
+	},
+	{
+		Name: "k9s",
+	},
+	{
+		Name: "git",
+	},
+	{
+		Name: "go",
+	},
+	{
+		Name: "aircrack-ng",
+	},
+	{
+		Name: "node",
+	},
+	{
+		Name: "docker",
+	},
+	{
+		Name: "python3",
+	},
+	{
+		Name: "python2",
+	},
+	{
+		Name: "kubectl",
+	},
+	{
+		Name: "rust",
+	},
+	{
+		Name: "dotnet",
+	},
+	{
+		Name: "java21",
+	},
+	{
+		Name: "bazel",
+	},
+}
